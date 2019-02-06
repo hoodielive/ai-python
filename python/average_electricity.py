c06e6dcb8bd0fd13c544b9b502abadb7ff84d443
hoodielive <hoodielive@gmail.com>
2019-02-05 20:03:27 -0500

    Calculate the average electricity costs

---
+++ python/average_electricity.py
@@ -0,0 +1,4 @@
+# An electricity bills for the last three months have been $23, $32 and $64. 
+# What is the average monthly electricity bill over the three month period?
+
+print((23 + 32 + 64) / 3)
