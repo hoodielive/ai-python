bfeeea498d0c3ecccbb8425b8e9569f7b0326571
hoodielive <hoodielive@gmail.com>
2019-02-08 19:54:48 -0500

    Tiler calculation

--- python/calculate_tiler.py
+++ python/calculate_tiler.py
@@ -1,9 +1,9 @@
 # In this quiz you're going to do some calculations for a tiler. 
 # Two parts of a floor need tiling. 
 # One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. 
+print(9*7 + 5*7)
+
 # Tiles come in packages of 6.
 # How many tiles are needed?
 # You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
-
-print()
-print()
+print(17*6 - (9*7 + 5*7))
